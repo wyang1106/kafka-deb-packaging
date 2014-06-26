7ebeae5e3ff00b18dc8cd119fcf68d4b459de243
fluential <michal@qubitdigital.com>
2014-06-26 16:08:40 +0100

    Small cleanup

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -u
-name=kafka
+name="kafka"
 version=${_KVERSION:-"0.8.1"}
 scala_version=${_KSCALAVERSION:-"2.10"}
 description="Apache Kafka is a distributed publish-subscribe messaging system."
@@ -10,10 +10,9 @@ arch="all"
 section="misc"
 license="Apache Software License 2.0"
 package_version=${_KPKGVERSION:-"-1"}
-src_package="kafka_${scala_version}-${version}.tgz"
+src_package="${name}_${scala_version}-${version}.tgz"
 download_url="https://dist.apache.org/repos/dist/release/kafka/${version}/${src_package}"
 origdir="$(pwd)"
-license="Apache Software License 2.0"
 
 function cleanup() {
     rm -rf ${name}*.deb
@@ -24,14 +23,14 @@ function bootstrap() {
         wget ${download_url}
     fi
     mkdir -p tmp && pushd tmp
-    rm -rf kafka
-    mkdir -p kafka
-    cd kafka
-    mkdir -p build/usr/lib/kafka
+    rm -rf ${name}
+    mkdir -p ${name}
+    cd ${name}
+    mkdir -p build/usr/lib/${name}
     mkdir -p build/etc/default
     mkdir -p build/etc/init
-    mkdir -p build/etc/kafka
-    mkdir -p build/var/log/kafka
+    mkdir -p build/etc/${name}
+    mkdir -p build/var/log/${name}
 }
 
 function build() {
@@ -39,11 +38,11 @@ function build() {
     #cp ${origdir}/kafka-broker.upstart.conf build/etc/init/kafka-broker.conf
     
     tar zxf ${origdir}/${src_package}
-    cd kafka_${scala_version}-${version}
-    cp -rp config/* ../build/etc/kafka
+    cd ${name}_${scala_version}-${version}
+    cp -rp config/* ../build/etc/${name}
     mv config config.old
-    cp ${origdir}/log4j.properties ../build/etc/kafka
-    mv * ../build/usr/lib/kafka
+    cp ${origdir}/log4j.properties ../build/etc/${name}
+    mv * ../build/usr/lib/${name}
 
     if [ -f ${origdir}/kafka-server-start.sh-${version} ]; then cp ${origdir}/kafka-server-start.sh-${version} ../build/usr/lib/kafka/bin/kafka-server-start.sh;fi
 
@@ -55,21 +54,20 @@ function mkdeb() {
     -n ${name} \
     -v ${version}${package_version} \
     --description "${description}" \
-    --url="{$url}" \
+    --url="${url}" \
     -a ${arch} \
     --category ${section} \
     --vendor "" \
-    --license "${license}" \
     --after-install ../../../kafka-broker.postinst \
     --after-remove  ../../../kafka-broker.postrm \
     --before-remove ../../../kafka-broker.prerm \
     -m "${USER}@${HOSTNAME}" \
     --deb-default ../../..//kafka-broker.default \
     --deb-upstart ../../..//kafka-broker \
-    --config-files /etc/kafka/log4j.properties \
-    --config-files /etc/kafka/server.properties \
-    --config-files /etc/kafka/producer.properties \
-    --config-files /etc/kafka/consumer.properties \
+    --config-files /etc/${name}/log4j.properties \
+    --config-files /etc/${name}/server.properties \
+    --config-files /etc/${name}/producer.properties \
+    --config-files /etc/${name}/consumer.properties \
     --license "${license}" \
     --prefix=/ \
     -s dir \
